f56f15e0d9bca2afc2d5747fde3da32f54d381ff
pickyourdestiny <avie.grinberg@gmail.com>
2024-05-29 23:14:06 -0400

    devextreme tabs repurposed as thumbnail viewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,182 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useState, useCallback, useEffect } from "react"
+import { Gallery } from "devextreme-react/gallery"
+import Tabs from "devextreme-react/tabs"
+import Button from "devextreme/ui/button"
+import pauseSVG from "./assets/icons/pause.svg"
+import pauseAccentSVG from "./assets/icons/pause-accent.svg"
+import playSVG from "./assets/icons/play.svg"
+import playAccentSVG from "./assets/icons/play-accent.svg"
+import { images } from "./assets/images"
+import { RadioGroup } from "devextreme-react/radio-group"
 
-function App() {
+export default function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [pausePlay, setPausePlay] = useState("play")
+  const [thumbnails, setThumbnails] = useState(images)
+  const [galleryImages, setGalleryImages] = useState(images)
+  const [thumbnailOrientation, setThumbnailOrientation] = useState("vertical")
+
+  const onThumbnailOrientationChange = useCallback((e) => {
+    setThumbnailOrientation(e.value)
+  }, [])
+
+  const onContentReady = useCallback(() => {
+    const imagesWrapper = document.querySelector(
+      "#images-gallery .dx-gallery-wrapper"
+    )
+    const pausePlayButtons = document.getElementById("pause-play-buttons")
+
+    if (!pausePlayButtons && imagesWrapper) {
+      const pausePlayElement = document.createElement("div")
+      const pauseElement = document.createElement("div")
+
+      const playElement = document.createElement("div")
+      pausePlayElement.setAttribute("id", "pause-play-buttons")
+      pausePlayElement.setAttribute("class", "dx-widget")
+
+      let playButtonInstance = new Button(playElement, {
+        icon: playSVG,
+        stylingMode: "text",
+        elementAttr: { id: "play-button" },
+        onClick: () => setPausePlay("play"),
+      })
+
+      let pauseButtonInstance = new Button(pauseElement, {
+        icon: pauseSVG,
+        stylingMode: "text",
+        elementAttr: { id: "pause-button" },
+        onClick: () => setPausePlay("pause"),
+      })
+
+      pausePlayElement.append(playElement)
+      pausePlayElement.append(pauseElement)
+      imagesWrapper.append(pausePlayElement)
+    }
+  }, [])
+
+  const onSelectionChanged = useCallback(
+    (e) => {
+      const newItem = e.addedItems[0]
+      galleryImages.forEach((item, index) => {
+        if (item === newItem) {
+          setSelectedIndex(index)
+        }
+      })
+    },
+    [galleryImages]
+  )
+
+  const renderThumbnail = useCallback(
+    (e) => {
+      if (thumbnails) {
+        return (
+          <div>
+            <img className={"thumbnail-image"} src={e} alt={""} />
+          </div>
+        )
+      }
+    },
+    [thumbnails]
+  )
+
+  useEffect(() => {
+    const playButtonElement = document.getElementById("play-button")
+    const playButtonInstance = Button.getInstance(playButtonElement)
+    const pauseButtonElement = document.getElementById("pause-button")
+    const pauseButtonInstance = Button.getInstance(pauseButtonElement)
+
+    if (pauseButtonInstance && playButtonInstance) {
+      //reset button styles
+      pauseButtonElement.style.border = ""
+      pauseButtonElement.style.backgroundColor = ""
+      playButtonElement.style.border = ""
+      playButtonElement.style.backgroundColor = ""
+
+      if (pausePlay === "play") {
+        playButtonInstance.option("icon", playAccentSVG)
+        playButtonElement.style.border = "1px dotted darkgrey"
+        playButtonElement.style.backgroundColor = "#ededed"
+
+        pauseButtonInstance.option("icon", pauseSVG)
+      } else {
+        playButtonInstance.option("icon", playSVG)
+        pauseButtonInstance.option("icon", pauseAccentSVG)
+        pauseButtonElement.style.border = "1px dotted darkgrey"
+        pauseButtonElement.style.backgroundColor = "#ededed"
+      }
+    }
+  }, [pausePlay])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='demo'>
+      <div className='flex mt-16'>
+        <div className='caption mb-8'>Select Thumbnail Orientation:</div>
+        <div className='mb-16 options'>
+          <RadioGroup
+            items={radioGroupItems}
+            layout={"horizontal"}
+            value={thumbnailOrientation}
+            onValueChanged={onThumbnailOrientationChange}
+          />
+        </div>
+      </div>
+      <div
+        className={
+          thumbnailOrientation === "vertical" ? "flex-row mt-16" : "flex mt-16"
+        }
+      >
+        {thumbnailOrientation === "vertical" && (
+          <div className={"flex mr-16"}>
+            <Tabs
+              items={thumbnails}
+              orientation='vertical'
+              showNavButtons={true}
+              itemRender={renderThumbnail}
+              height={galleryHeight}
+              selectedIndex={selectedIndex}
+              onSelectionChanged={onSelectionChanged}
+              loop={true}
+            />
+          </div>
+        )}
+        <Gallery
+          elementAttr={elementAttr}
+          showIndicator={false}
+          selectedIndex={selectedIndex}
+          dataSource={galleryImages}
+          showNavButtons={true}
+          animationEnabled={true}
+          width={galleryWidth}
+          height={galleryHeight}
+          animationDuration={animationDuration}
+          onSelectionChanged={onSelectionChanged}
+          onContentReady={onContentReady}
+          slideshowDelay={pausePlay === "pause" ? 0 : slideShowDelay}
+          loop={true}
+        />
+        {thumbnailOrientation === "horizontal" && (
+          <div className={"flex mt-8 mb-8"}>
+            <Tabs
+              items={thumbnails}
+              orientation='horizontal'
+              showNavButtons={true}
+              itemRender={renderThumbnail}
+              width={galleryWidth}
+              selectedIndex={selectedIndex}
+              onSelectionChanged={onSelectionChanged}
+              loop={true}
+            />
+          </div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const animationDuration = 1000 //ms
+const slideShowDelay = 3000 //ms
+const elementAttr = { id: "images-gallery" }
+const galleryHeight = "450px"
+const galleryWidth = "600px"
+const radioGroupItems = ["vertical", "horizontal"]
---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import image1 from "./1.jpg"
+import image2 from "./2.jpg"
+import image3 from "./3.jpg"
+import image4 from "./4.jpg"
+import image5 from "./5.jpg"
+import image6 from "./6.jpg"
+import image7 from "./7.jpg"
+import image8 from "./8.jpg"
+import image9 from "./9.jpg"
+
+export const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+]
